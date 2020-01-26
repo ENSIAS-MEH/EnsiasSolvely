dd97d47e19480758df5a1d9802978d6bed3f4b11
TenniaYoussef <59965420+TenniaYoussef@users.noreply.github.com>
2020-01-26 22:29:47 +0100

    Create AboutUsS.java

---
+++ EnsiasSolvelyJEE/src/com/EnsiasSolvely/servlets/AboutUsS.java
@@ -0,0 +1,34 @@
+package com.EnsiasSolvely.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AboutUsS
+ */
+@WebServlet("/AboutUsS")
+public class AboutUsS extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AboutUsS() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		this.getServletContext().getRequestDispatcher("/WEB-INF/AboutUs.jsp").forward(request, response);
+	}
+
+
+}

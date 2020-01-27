f2a8a24de7d78b56b635d0642ec662b8e49d558a
younessmouad <ymouad96@gmail.com>
2020-01-27 20:02:38 +0100

    Update ProblemsS.java

--- EnsiasSolvelyJEE/src/com/EnsiasSolvely/servlets/ProblemsS.java
+++ EnsiasSolvelyJEE/src/com/EnsiasSolvely/servlets/ProblemsS.java
@@ -6,7 +6,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import com.EnsiasSolvely.BeanForm.EleveBEAN;
+import com.EnsiasSolvely.BeanForm.ProblemsBEAN;
+import com.EnsiasSolvely.BeanForm.ProblemsForm;
+import com.EnsiasSolvely.DAO.ProblemsDAO;
 
 /**
  * Servlet implementation class ProblemsS
@@ -27,7 +32,10 @@ public class ProblemsS extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		HttpSession session = request.getSession();
+        EleveBEAN eleve = (EleveBEAN) session.getAttribute("eleve");
+		ProblemsBEAN[] problems = ProblemsDAO.getActifProblems(eleve.getId_comite());
+		request.setAttribute("problems", problems);
 		this.getServletContext().getRequestDispatcher("/WEB-INF/Problems.jsp").forward(request, response);
 	}
 
@@ -35,7 +43,13 @@ public class ProblemsS extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ProblemsForm problemsForm = new ProblemsForm();
 		
+		problemsForm.changeStatut(request);
+		if(!problemsForm.settings.getErreurs().isEmpty()) {
+			request.setAttribute("form", problemsForm.settings);
+		}
+		doGet(request, response);
 	}
 
 }
